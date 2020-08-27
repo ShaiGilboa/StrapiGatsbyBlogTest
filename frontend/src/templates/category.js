d71d17bb8962ff1b790e0026f3e18e0d1bb63168
Shai Gilboa <shizel@gmail.com>
2020-08-27 17:48:46 -0400

    finished toturial

--- frontend/src/templates/category.js
+++ frontend/src/templates/category.js
@@ -1,45 +1,46 @@
-// import React from "react"
-// import { graphql } from "gatsby"
+import React from "react"
+import { graphql } from "gatsby"
 
-// import ArticlesComponent from "../components/articles"
-// import Layout from "../components/layout"
+import ArticlesComponent from "../components/articles"
+import Layout from "../components/layout"
 
-// export const query = graphql`
-//   query Category($id: Int!) {
-//     articles: allStrapiArticle(filter: { category: { id: { eq: $id } } }) {
-//       edges {
-//         node {
-//           strapiId
-//           title
-//           category {
-//             name
-//           }
-//           image {
-//             url
-//           }
-//         }
-//       }
-//     }
-//     category: strapiCategory(strapiId: { eq: $id }) {
-//       name
-//     }
-//   }
-// `
+export const query = graphql`
+  query Category($id: Int!) {
+    articles: allStrapiArticle(filter: { category: { id: { eq: $id } } }) {
+      edges {
+        node {
+          strapiId
+          title
+          category {
+            name
+          }
+          image {
+            publicURL
+          }
+        }
+      }
+    }
+    category: strapiCategory(strapiId: { eq: $id }) {
+      name
+    }
+  }
+`
 
-// const Category = ({ data }) => {
-//   const articles = data.articles.edges
-//   const category = data.category.name
 
-//   return (
-//     <Layout>
-//       <div className="uk-section">
-//         <div className="uk-container uk-container-large">
-//           <h1>{category}</h1>
-//           <ArticlesComponent articles={articles} />
-//         </div>
-//       </div>
-//     </Layout>
-//   )
-// }
+const Category = ({ data }) => {
+  const articles = data.articles.edges
+  const category = data.category.name
 
-// export default Category
+  return (
+    <Layout>
+      <div className="uk-section">
+        <div className="uk-container uk-container-large">
+          <h1>{category}</h1>
+          <ArticlesComponent articles={articles} />
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default Category
